01470ba68d7bbb1f219baf0134ad3656e97d279b
ericcapiz <ericcapiz@gmail.com>
2021-05-04 13:10:10 -0500

    added styling and started nav

--- app.js
+++ app.js
@@ -5,15 +5,17 @@ const app = express();
 //Templating engine as ejs
 app.set('view engine', 'ejs');
 
+//Serving Static Files
+app.use(express.static('public'))
+
 //ROUTING
 
-//Route for /
+//Route for home page
 app.get('/',(req,res)=>{
     res.render('Home');
 })
 
 //Route for about page
-
 app.get('/about',(req,res)=>{
     res.render("About")
 })
