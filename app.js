9b61f5f6c2f08a42f792dbd9c1c913bc8d9199e1
ericcapiz <ericcapiz@gmail.com>
2021-05-23 12:58:08 -0500

    added mongoose

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const express = require('express')
+const express = require('express');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
-
+dotenv.config();
 //Templating engine as ejs
 app.set('view engine', 'ejs');
 
 //Serving Static Files
 app.use(express.static('public'))
+
+//BodyParser
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+//Connect to mongoose
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true,  useUnifiedTopology: true, useCreateIndex: true},() =>{
+    console.log("Connected to MongoDB")
+})
+
 
 //ROUTING
 
@@ -25,10 +38,17 @@ app.get('/diary',(req,res)=>{
     res.render("Diary")
 })
 
-//Route for adding records
+//Route for adding to diary
 app.get('/add',(req,res)=>{
     res.render("add")
 })
 
+//Route for saving diary
+app.post('/add-to-diary',(req,res)=>{
+    res.send(req.body.title)
+})
+
+
 //Create Server
 app.listen(3000, () => console.log('server running...'))
+
